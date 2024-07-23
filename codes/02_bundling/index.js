96759e2cbbd751bebb9fad827d03be03f7de3d47
iam-abhishek-yadav <abhrecscs17@gmail.com>
2024-07-23 17:17:30 +0530

    Update: codes/bundling

--- codes/02_bundling/index.js
+++ codes/02_bundling/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement('h1', { id: 'heading1' }, 'Hello world!');
+// React element ==> Object ==> HTMLelement (on render)
 
-const parent = React.createElement(
-	'div',
-	{ id: 'parent' },
-	React.createElement('div', { id: 'child' }, heading)
+// const heading = React.createElement('h1', { id: 'heading' }, 'Hello world!');
+
+// JSX (transpiled to React.createElement by Babel)
+
+const heading = <h2 id='heading'>Hello World!</h2>;
+
+// Component
+
+const Title = () => <h1 id='title'>Title Component!</h1>;
+
+const subTitle = <h3 id='title'>subTitle!</h3>;
+
+const HeadingComponent = () => (
+	<div id='component'>
+		<Title />
+		{Title()}
+		<Title></Title>
+		<h3>{2 + 2}</h3>
+		<h3>{subTitle}</h3>
+		<h2 style={{ marginLeft: '20px' }}>Hello Component!</h2>
+	</div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+
+// root.render(heading);
+root.render(<HeadingComponent />);
